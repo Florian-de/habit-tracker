9a97306ec3c0268061e3d4201697cdef9a50603c
Florian Dreyer <64322175+Florian-de@users.noreply.github.com>
2022-09-12 17:11:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+from datetime import datetime
+
+USERNAME = "floriand"
+TOKEN = "hsiu398dhfiuh7Z397BIHAD9"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+
+#response = requests.post(url=pixela_endpoint, json=user_params)
+#print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "day1",
+    "name": "Coding",
+    "unit": "Minutes",
+    "type": "int",
+    "color": "shibafu"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+#response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+#print(response.text)
+
+date = datetime.now().strftime("%Y%m%d")
+
+graph_add_endpoint = "https://pixe.la/v1/users/floriand/graphs/day1"
+add_params = {
+    "date": date,
+    "quantity": "600"
+}
+
+response = requests.post(url=graph_add_endpoint, json=add_params, headers=headers)
+print(response.text)
